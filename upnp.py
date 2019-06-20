dda47122546cb8c219501b6adbc1590da3da0a41
Walter Mitty <gsitcia@gmail.com>
2019-06-20 17:39:40 -0400

    Update upnp.py

--- upnp.py
+++ upnp.py
@@ -1,55 +1,3 @@
-import socket
-
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
-
-SSDP_ADDR = "239.255.255.250"
-SSDP_PORT = 1900
-SSDP_MX = 2
-SSDP_ST = "urn:schemas-upnp-org:device:InternetGatewayDevice:1"
-
-ssdpRequest = "M-SEARCH * HTTP/1.1\r\n" + \
-                "HOST: %s:%d\r\n" % (SSDP_ADDR, SSDP_PORT) + \
-                "MAN: \"ssdp:discover\"\r\n" + \
-                "MX: %d\r\n" % (SSDP_MX, ) + \
-                "ST: %s\r\n" % (SSDP_ST, ) + "\r\n"
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(str.encode(ssdpRequest), (SSDP_ADDR, SSDP_PORT))
-resp = sock.recv(1000)
-
-import re
-from urllib import urlparse as parse
-
-parsed = re.findall(r'(?P<name>.*?): (?P<value>.*?)\r\n', resp.decode('utf-8'))
-# get the location header
-location = list(filter(lambda x: x[0].lower() == "location", parsed))
-# use the urlparse function to create an easy to use object to hold a URL
-router_path = parse.urlparse(location[0][1])
-
-from urllib2 import urlopen
-from xml.dom.minidom import parseString
-# get the profile xml file and read it into a variable
-directory = urlopen(location[0][1]).read()
-
-# create a DOM object that represents the `directory` document
-dom = parseString(directory)
-
-# find all 'serviceType' elements
-service_types = dom.getElementsByTagName('serviceType')
-
-# iterate over service_types until we get either WANIPConnection
-# (this should also check for WANPPPConnection, which, if I remember correctly
-# exposed a similar SOAP interface on ADSL routers.
-for service in service_types:
-    # I'm using the fact that a 'serviceType' element contains a single text node, who's data can
-    # be accessed by the 'data' attribute.
-    # When I find the right element, I take a step up into its parent and search for 'controlURL'
-    if service.childNodes[0].data.find('WANIPConnection') > 0:
-        path= str(service.parentNode.getElementsByTagName('controlURL')[0].childNodes[0].data)
-        #path = service.parentNode.getElementsByTagName('controlURL')[0].childNodes[0].data)
 
 from xml.dom.minidom import Document
 
@@ -112,27 +60,19 @@ doc.appendChild(envelope)
 # our tree is ready, conver it to a string
 pure_xml = doc.toxml()
 
-import http.client
+import requests
 
 # use the object returned by urlparse.urlparse to get the hostname and port
 print(pure_xml)
-conn = http.client.HTTPConnection(router_path.hostname, router_path.port)
 
-# use the path of WANIPConnection (or WANPPPConnection) to target that service,
-# insert the xml payload,
-# add two headers to make tell the server what we're sending exactly.
-conn.request('POST',
-    path,
-    pure_xml,
-    {'SOAPAction': '"urn:schemas-upnp-org:service:WANIPConnection:1#AddPortMapping"',
-     'Content-Type': 'text/xml'}
-)
+path = http://192.168.1.1:2555/upnp/445269bc-ed9f-39d6-a86b-a975069c1d8d/WANIPConn1.ctl
 
-# wait for a response
-resp = conn.getresponse()
+resp = requests.post(path,data=pure_xml,
+    headers={'SOAPAction': '"urn:schemas-upnp-org:service:WANIPConnection:1#AddPortMapping"',
+     'Content-Type': 'text/xml'})
 
 # print the response status
 print(resp.status)
 
 # print the response body
-print(resp.read())
+print(resp.text)
