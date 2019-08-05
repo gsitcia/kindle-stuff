41f8d325c24df719adc0fabf174a53635ce2d43e
Walter Mitty <gsitcia@gmail.com>
2019-08-05 01:13:43 -0500

    test

--- startup.sh
+++ startup.sh
@@ -5,3 +5,4 @@ tar -C ~/opt/cross-gcc-linaro/arm-linux-gnueabi/ -xf khdrs.tar.gz
 touch ~/.bash_aliases
 echo "alias kindle-gcc=~/opt/cross-gcc-linaro/bin/arm-linux-gnueabi-gcc" >> ~/.bash_aliases
 source ~/.bashrc
+# test
